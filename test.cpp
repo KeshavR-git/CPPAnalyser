57f93621dda5b8e575d7698f3b9625ef778c4fb3
kr-1662 <a1834181@adelaide.edu.au>
2023-12-08 00:13:42 +1030

    Implemented Rule10, working with basic testing

--- test.cpp
+++ test.cpp
@@ -2,12 +2,27 @@
 #include <string>
 
 int main() {
-    // Pattern 10
+    int x;
 
+    // Pattern 5
+
+    if (x == 0) {
+
+    } else if (x != 0) {
+
+    }
+
+    // Pattern 10
     for (int i = 0; i < 10; i++) {
         std::string* input = new std::string("Repeated String");
         std::cout << *input << std::endl;
         delete input;
+    }
+
+    for (int i = 0; i < 10; i++) {
+        int* num = new int(5);
+        std::cout << *num << std::endl;
+        delete num;
     }
 
     return 0;
