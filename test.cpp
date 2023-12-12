cab7ee2ece3dcdc0808d7ff1222f298cd6e018f4
kr-1662 <a1834181@adelaide.edu.au>
2023-12-12 17:04:02 +1030

    added one more test case in test.cpp for testing Rule12, working

--- test.cpp
+++ test.cpp
@@ -1,27 +1,47 @@
 #include <iostream>
 #include <string>
 
-int main() {
+int main()
+{
     int end = 5;
     // Pattern 12 - Variation 1
-    for (int i = 0; i < end; i++) {
-        if (i == end-1) {
+    for (int i = 0; i < end; i++)
+    {
+        if (i == end - 1)
+        {
             std::cout << "i == end - 1";
         }
-        else {
+        else
+        {
             std::cout << "i != end - 1";
         }
     }
 
     // Pattern 12 - Variation 2
-    for (int i = 0; i < end; i++) {
-        if (i == 0) {
-            std::cout << "i == end - 1";
+    for (int i = 0; i < end; i++)
+    {
+        if (i == 0)
+        {
+            std::cout << "i == 0";
         }
-        else {
-            std::cout << "i != end - 1";
+        else
+        {
+            std::cout << "i != 0";
         }
-    } 
+    }
+
+    // Pattern 12 - Variation 2
+    for (int i = 1; i < end; i++)
+    {
+        if (i == 1)
+        {
+            std::cout << "i == 1";
+        }
+        else
+        {
+            std::cout << "i != 1";
+        }
+    }
 
     return 0;
 }
